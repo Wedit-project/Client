5b99d76bb46b90f4d7744441a81dc60511e7b8df
rhdmschdl <lovelyskygir@naver.com>
2025-01-11 16:10:41 +0900

    style: 옵션 선택 페이지 UI 구현

--- Wedit-Client/src/pages/OptionSelectionPage.jsx
+++ Wedit-Client/src/pages/OptionSelectionPage.jsx
@@ -22,9 +22,11 @@ const OptionSelectionPage = () => {
 		<Wrapper>
 			<LogoComponent />
 			<TitleBox>옵션 선택</TitleBox>
+			<DescriptionSpan>1개 이상의 옵션을 선택해주세요.</DescriptionSpan>
 			<ContentContainer>
 				<OptSelection />
 			</ContentContainer>
+			<CautionBox>1개 의상의 옵션을 선택을 해주세요!</CautionBox>
 			<NavBox>
 				<NavButton
 					onPrevious={handlePrevious}
@@ -52,12 +54,43 @@ const TitleBox = styled.div`
 	letter-spacing: -0.608px;
 `;
 
+const DescriptionSpan = styled.div`
+	position: absolute;
+	top: 17.6rem;
+	left: 8.3rem;
+	width: 76.1rem;
+	color: var(--black, #000);
+	font-family: Pretendard;
+	font-size: 2.4rem;
+	font-style: normal;
+	font-weight: ${theme.font.medium.fontWeight};
+	line-height: 3.2rem;
+	letter-spacing: -0.456px;
+`;
+
 const ContentContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	position: relative;
+`;
+
+const CautionBox = styled.div`
+	position: absolute;
+	top: 83.2rem;
+	left: 55.7rem;
+	width: 32.6rem;
+	height: 4.1rem;
+	/* margin: 4.845rem 57.3rem 2.9rem 57.4rem; */
+	color: #e80c0c;
+	font-family: Pretendard;
+	font-size: 2.4rem;
+	font-style: normal;
+	font-weight: ${theme.font.medium.fontWeight};
+	line-height: 3.2rem;
+	letter-spacing: -0.456px;
 `;
 
 const NavBox = styled.div`
-	margin: 1.4rem 50.3rem 6.4rem 50.1rem;
+	margin: 2.4rem 50.3rem 6.4rem 50.1rem;
 `;
