182dd4f517ee9d3f30614ae415099562af7fe450
lyn0719 <usolnim@naver.com>
2025-01-12 06:11:49 +0900

    feat: 메인페이지 헤더 구현

---
+++ Wedit-Client/src/components/mainpage/LoginButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import theme from "../../styles/theme";
+
+export const LoginButton = ({ className }) => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <Button className={className} onClick={handleLoginClick}>
+      로그인
+    </Button>
+  );
+};
+
+// CSS
+const Button = styled.button`
+  border: 0.1rem solid;
+  border-radius: 3rem;
+  padding: 0.6rem 1.9rem;
+  font-size: ${theme.fontSize.xlarge};
+  font-weight: ${theme.font.semibold.fontWeight};
+  cursor: pointer;
+  background-color: transparent;
+`;
--- Wedit-Client/src/pages/HomePage.jsx
+++ Wedit-Client/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import PreviewBox from "../components/mainpage/PreviewBox";
 import MainBanner from '../components/mainpage/MainBanner';
+import MainPageHeader from '../components/mainpage/MainPageHeader';
 
 const HomePage = () => {
 	return (
 		<HomePageContainer>
+			<MainPageHeader />
 			<MainBanner />
 			<PreviewBox />
 		</HomePageContainer>
