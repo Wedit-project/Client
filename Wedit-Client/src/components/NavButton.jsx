fe4f1025d13a4afb7ade481b735d2dde4345942d
rhdmschdl <lovelyskygir@naver.com>
2025-01-12 16:50:57 +0900

    style: 페이지 이동 버튼 컴포넌트 구현

--- Wedit-Client/src/components/NavButton.jsx
+++ Wedit-Client/src/components/NavButton.jsx
@@ -4,32 +4,48 @@ import styled from 'styled-components';
 const NavButton = ({ onPrevious, onNext, isNextActive }) => {
 	return (
 		<ButtonBox>
-			<Button onClick={onPrevious} isActive={true}>
+			<PrevButton onClick={onPrevious} isActive={true}>
 				이전
-			</Button>
-			<Button onClick={onNext} isActive={isNextActive}>
+			</PrevButton>
+			<NextButton onClick={onNext} isActive={isNextActive}>
 				다음
-			</Button>
+			</NextButton>
 		</ButtonBox>
 	);
 };
 
 const ButtonBox = styled.div`
 	display: flex;
-	width: 43.8rem;
+	width: 43.6rem;
 	height: 5.8rem;
 	justify-content: center;
 	align-items: flex-start;
-	gap: 4.2rem;
+	gap: 4rem;
 `;
 
-const Button = styled.button`
+const PrevButton = styled.button`
 	width: 19.8rem;
 	padding: 1.3rem 7.7rem;
 	border: none;
 	border-radius: 1.6rem;
-	background: ${(props) => (props.isActive ? '#acb66d' : '#d9d9d9')};
-	color: ${(props) => (props.isActive ? '#fff' : '#000')};
+	background: #e4e4e4;
+	color: #000000;
+	text-align: center;
+	font-family: Pretendard;
+	font-size: 2.4rem;
+	font-weight: 600;
+	line-height: 3.2rem;
+	letter-spacing: -0.456px;
+`;
+
+const NextButton = styled.button`
+	width: 19.8rem;
+	padding: 1.3rem 7.7rem;
+	border: none;
+	border-radius: 1.6rem;
+	background: ${(props) =>
+		props.isActive ? '#acb66d' : '#ACB66D80'}; // 활성화 상태에 따라 색상 변경
+	color: #ffffff;
 	text-align: center;
 	font-family: Pretendard;
 	font-size: 2.4rem;
