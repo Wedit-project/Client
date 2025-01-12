5e31b62b48e88c8beb5373b6514cf64fc3213143
lyn0719 <usolnim@naver.com>
2025-01-12 09:15:18 +0900

    feat: 마이페이지 헤더 구현

---
+++ Wedit-Client/src/components/mypage/HomeButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+const HomeButton = () => {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate("/");
+  };
+
+  return <StyledButton onClick={goToHome}>홈</StyledButton>;
+};
+
+export default HomeButton;
+
+// 스타일 정의
+const StyledButton = styled.button`
+    font-size: ${theme.fontSize.xlarge};
+    font-weight: ${theme.font.medium.fontWeight};
+    border: none;
+    background-color: transparent;
+`;
