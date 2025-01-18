16080683a60a6ea683a8b8bb3b447a57d6493ba5
rhdmschdl <lovelyskygir@naver.com>
2025-01-18 12:37:20 +0900

    feat: 사진 선택 페이지 인디케이터

---
+++ Wedit-Client/src/components/PhotoSelectionPage/PageIndicator.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+import CircleGray from '../../assets/img/Ellipse14.svg?react';
+
+const PageIndicator = ({ step }) => {
+	return (
+		<Container>
+			<CircleIcon isActive={step === 0} />
+			<CircleIcon isActive={step === 1} />
+			<CircleIcon isActive={step === 2} />
+			<CircleIcon isActive={step === 3} />
+		</Container>
+	);
+};
+
+export default PageIndicator;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: row;
+	gap: 1.7rem;
+`;
+
+const CircleIcon = styled(CircleGray)`
+	width: 1.8rem;
+	height: 1.8rem;
+	fill: ${(props) =>
+		props.isActive ? `${theme.colors.green['main']}` : `${theme.colors.gray['300']}`};
+`;
