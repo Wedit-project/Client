90dadfcfeb14a2d851a1a8ecdec33ef9d6a4d729
lyn0719 <usolnim@naver.com>
2025-01-25 07:39:33 +0900

    feat: 배너 스켈레톤 UI 구현

---
+++ Wedit-Client/src/components/mainpage/Skeleton.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+// 스켈레톤 UI 컴포넌트
+const Skeleton = () => {
+  return (
+    <SkeletonWrapper>
+      <SkeletonImage />
+    </SkeletonWrapper>
+  );
+};
+
+// 스켈레톤 UI 스타일
+const SkeletonWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${props => props.theme.colors.gray[200]};
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SkeletonImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%);
+  background-size: 200% 100%;
+  animation: loading 1.5s infinite linear;
+  border-radius: 8px;
+  
+  @keyframes loading {
+    0% {
+      background-position: 200% 0;
+    }
+    100% {
+      background-position: -200% 0;
+    }
+  }
+`;
+
+export default Skeleton;
