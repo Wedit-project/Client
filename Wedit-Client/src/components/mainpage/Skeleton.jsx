e93b63b0084ea948e7a9e5f7b20c9964d53606c2
lyn0719 <usolnim@naver.com>
2025-01-25 07:44:10 +0900

    스켈레톤 css 수정

--- Wedit-Client/src/components/mainpage/Skeleton.jsx
+++ Wedit-Client/src/components/mainpage/Skeleton.jsx
@@ -15,7 +15,6 @@ const SkeletonWrapper = styled.div`
   width: 100%;
   height: 100%;
   background-color: ${props => props.theme.colors.gray[200]};
-  border-radius: 8px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -27,7 +26,6 @@ const SkeletonImage = styled.div`
   background: linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%);
   background-size: 200% 100%;
   animation: loading 1.5s infinite linear;
-  border-radius: 8px;
   
   @keyframes loading {
     0% {
