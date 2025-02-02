ccdf9724c80e09fac34eb58b8780b0f1def3f42b
lyn0719 <usolnim@naver.com>
2025-02-02 09:19:24 +0900

    feat: 쿠키 설정, https 설정

--- src/store/authState.js
+++ src/store/authState.js
@@ -1,6 +1,16 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "localStorage", // 로컬스토리지에 데이터를 저장
+  storage: localStorage,
+});
 
 export const authState = atom({
   key: "authState",
-  default: null, // 초기값: 토큰 없음
+  default: {
+    success: null,
+    isLoggedIn: false,
+  }, // 기본값은 로그인되지 않은 상태
+  effects_UNSTABLE: [persistAtom],
 });
