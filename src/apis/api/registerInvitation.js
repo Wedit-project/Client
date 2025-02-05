b45ee1a7091bd1cc1b32f40ef860d1850d8afbce
rhdmschdl <lovelyskygir@naver.com>
2025-02-05 22:59:40 +0900

    fix: 폼데이터 정의 추가

--- src/apis/api/registerInvitation.js
+++ src/apis/api/registerInvitation.js
@@ -1,21 +1,43 @@
 import { serverInstance } from '../utils/apiIntance.js';
 
-const registerInvitation = async (images, content, token) => {
+const registerInvitation = async (images, content) => {
 	const url = `/api/invitations`;
 
 	const formData = new FormData();
-	images.forEach((image) => {
+
+	Array.from(images).forEach((image) => {
 		formData.append('images', image);
 	});
 
-	formData.append('content', JSON.stringify(content));
+	formData.append('content', new Blob([JSON.stringify(content)], { type: 'application/json' }));
+
+	let totalSize = 0;
+	for (let [key, value] of formData.entries()) {
+		if (value instanceof File) {
+			totalSize += value.size;
+		}
+	}
+	console.log('Total size of images:', totalSize);
 
 	try {
+		for (const x of formData) {
+			console.log(x);
+		}
+
+		const contentBlob = formData.get('content');
+		const reader = new FileReader();
+
+		reader.onload = function () {
+			console.log('Content:', JSON.parse(reader.result));
+		};
+
+		reader.readAsText(contentBlob);
+
 		const { data } = await serverInstance.post(url, formData, {
 			headers: {
 				'Content-Type': 'multipart/form-data',
-				Authorization: `Bearer ${token}`,
 			},
+			withCredentials: true,
 		});
 		return data.success.data;
 	} catch (error) {
