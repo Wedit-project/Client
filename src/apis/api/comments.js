5a72380c27d8fad8e5db8cb4f5257ea590016fb8
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-02-09 17:52:59 +0900

    feat : 방명록 조회 API 연결

--- src/apis/api/comments.js
+++ src/apis/api/comments.js
@@ -1,5 +1,6 @@
 import { serverInstance } from '../utils/apiIntance';
 
+// 방명록 등록
 export const registerGuestBook = async ({ name, content, invitationId }) => {
 	try {
 		const response = await serverInstance.post(
@@ -15,6 +16,21 @@ export const registerGuestBook = async ({ name, content, invitationId }) => {
 				},
 			}
 		);
+		console.log(response.data);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// 방명록 조회
+export const fetchGuestBook = async (invitationId, page) => {
+	try {
+		const response = await serverInstance.get(`/api/comments/${invitationId}?page=${page}`, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 		console.log(response.data);
 		return response.data;
 	} catch (error) {
