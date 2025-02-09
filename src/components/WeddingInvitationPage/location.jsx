215b8fcb40d56bc9b58279ae99abd0194c428197
lyn0719 <usolnim@naver.com>
2025-02-09 18:45:05 +0900

    비회원 청첩장 조회 API 연결

--- src/components/WeddingInvitationPage/location.jsx
+++ src/components/WeddingInvitationPage/location.jsx
@@ -5,104 +5,104 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import useKakaoLoader from '../../hooks/useKakaoLoader';
 
 const Location = ({ $variant = 'basic', invitationData }) => {
-    useKakaoLoader();
+	useKakaoLoader();
 
-    const [location, setLocation] = useState({ lat: 33.450701, lng: 126.570667 }); // 지도 중심좌표
-    const [isMapLoaded, setIsMapLoaded] = useState(false);
+	const [location, setLocation] = useState({ lat: 33.450701, lng: 126.570667 }); // 지도 중심좌표
+	const [isMapLoaded, setIsMapLoaded] = useState(false);
 
-    useEffect(() => {
-        console.log("주소 변경됨:", invitationData.address);
+	useEffect(() => {
+		console.log('주소 변경됨:', invitationData.address);
 
-        // SDK가 로드될 때까지 기다리기
-        const checkKakaoLoaded = setInterval(() => {
-            if (window.kakao && window.kakao.maps && window.kakao.maps.services) {
-                clearInterval(checkKakaoLoaded);
-                setIsMapLoaded(true);
-            }
-        }, 500);
+		// SDK가 로드될 때까지 기다리기
+		const checkKakaoLoaded = setInterval(() => {
+			if (window.kakao && window.kakao.maps && window.kakao.maps.services) {
+				clearInterval(checkKakaoLoaded);
+				setIsMapLoaded(true);
+			}
+		}, 500);
 
-        return () => clearInterval(checkKakaoLoaded);
-    }, []);
+		return () => clearInterval(checkKakaoLoaded);
+	}, []);
 
-    useEffect(() => {
-        if (!isMapLoaded) return; // SDK가 로드되지 않으면 실행X
+	useEffect(() => {
+		if (!isMapLoaded) return; // SDK가 로드되지 않으면 실행X
 
-        const geocoder = new window.kakao.maps.services.Geocoder();
+		const geocoder = new window.kakao.maps.services.Geocoder();
 
-        // 주소-좌표 변환
-        geocoder.addressSearch(invitationData.address, (result, status) => {
-            if (status === window.kakao.maps.services.Status.OK) {
-                setLocation({
-                    lat: parseFloat(result[0].y),
-                    lng: parseFloat(result[0].x)
-                });
-            } else {
-                console.error("주소 변환 실패");
-            }
-        });
-    }, [isMapLoaded, invitationData.address]);
+		// 주소-좌표 변환
+		geocoder.addressSearch(invitationData.address, (result, status) => {
+			if (status === window.kakao.maps.services.Status.OK) {
+				setLocation({
+					lat: parseFloat(result[0].y),
+					lng: parseFloat(result[0].x),
+				});
+			} else {
+				console.error('주소 변환 실패');
+			}
+		});
+	}, [isMapLoaded, invitationData.address]);
 
-    return (
-        <LocationWrapper>
-            <LocationSpan $variant={$variant}>위치</LocationSpan>
-            {isMapLoaded && (
-                <MapBox center={location} level={3}>
-                    <MapMarker position={location} />
-                </MapBox>
-            )}
-        </LocationWrapper>
-    );
+	return (
+		<LocationWrapper>
+			<LocationSpan $variant={$variant}>위치</LocationSpan>
+			{isMapLoaded && (
+				<MapBox center={location} level={3}>
+					<MapMarker position={location} />
+				</MapBox>
+			)}
+		</LocationWrapper>
+	);
 };
 
 export default Location;
 
 // CSS
 const LocationWrapper = styled.div`
-    margin-top: 10.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+	margin-top: 10.5rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 const LocationSpan = styled.span`
-    font-weight: ${theme.font.bold.fontWeight};
-    color: ${theme.colors.green.main};
-    font-size: 4.4rem;
-    font-style: normal;
-    line-height: 66.482%;
-    letter-spacing: -0.0836rem;
+	font-weight: ${theme.font.bold.fontWeight};
+	color: ${theme.colors.green.main};
+	font-size: 4.4rem;
+	font-style: normal;
+	line-height: 66.482%;
+	letter-spacing: -0.0836rem;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: relative;
 
-    ${({ $variant }) =>
-        $variant === 'tradition' &&
-        css`
-            color: ${theme.colors.traditionalWedding.invitation5};
-            &::before,
-            &::after {
-                content: '';
-                display: block;
-                width: 49rem;
-                height: 0.2rem;
-                background-color: ${theme.colors.traditionalWedding.invitation5};
-            }
+	${({ $variant }) =>
+		$variant === 'tradition' &&
+		css`
+			color: ${theme.colors.traditionalWedding.invitation5};
+			&::before,
+			&::after {
+				content: '';
+				display: block;
+				width: 49rem;
+				height: 0.2rem;
+				background-color: ${theme.colors.traditionalWedding.invitation5};
+			}
 
-            &::before {
-                margin-right: 7.1rem;
-            }
+			&::before {
+				margin-right: 7.1rem;
+			}
 
-            &::after {
-                margin-left: 7.1rem;
-            }
-        `}
+			&::after {
+				margin-left: 7.1rem;
+			}
+		`}
 `;
 
 const MapBox = styled(Map)`
-    margin-top: 3.9rem;
-    width: 112.1rem;
-    height: 50.2rem;
-    border-radius: 2rem;
+	margin-top: 3.9rem;
+	width: 112.1rem;
+	height: 50.2rem;
+	border-radius: 2rem;
 `;
