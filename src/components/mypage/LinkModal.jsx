d85ccfd49335a68cd6ae5e289602b855c1ead197
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-02-15 02:45:49 +0900

    style : 배포 링크 모달 반응형 font size 수정

--- src/components/mypage/LinkModal.jsx
+++ src/components/mypage/LinkModal.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import styled from "styled-components";
-import theme from "../../styles/theme";
-import CloseIcon from "../../assets/icons/close.svg?react";
+import React from 'react';
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+import CloseIcon from '../../assets/icons/close.svg?react';
 
 const LinkModal = ({ url, onClose }) => {
-  const handleCopy = () => {
-    navigator.clipboard.writeText(url);
-    alert("링크가 복사되었습니다!");
-  };
+	const handleCopy = () => {
+		navigator.clipboard.writeText(url);
+		alert('링크가 복사되었습니다!');
+	};
 
-  return (
-    <ModalOverlay>
-      <ModalContainer onClick={(e) => e.stopPropagation()}>
-        <ModalHeader>
-            <Header>배포 링크</Header>
-            <StyledCloseIcon onClick={onClose} />
-        </ModalHeader>
-        <Content>
-          <LinkText>{url}</LinkText>
-          <CopyButton onClick={handleCopy}>복사하기</CopyButton>
-        </Content>
-      </ModalContainer>
-    </ModalOverlay>
-  );
+	return (
+		<ModalOverlay>
+			<ModalContainer onClick={(e) => e.stopPropagation()}>
+				<ModalHeader>
+					<Header>배포 링크</Header>
+					<StyledCloseIcon onClick={onClose} />
+				</ModalHeader>
+				<Content>
+					<LinkText>{url}</LinkText>
+					<CopyButton onClick={handleCopy}>복사하기</CopyButton>
+				</Content>
+			</ModalContainer>
+		</ModalOverlay>
+	);
 };
 
 export default LinkModal;
@@ -30,74 +30,87 @@ export default LinkModal;
 // CSS
 
 const ModalOverlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.3);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 9999;
 `;
 
 const ModalContainer = styled.div`
-    width: 90.3rem;
-    hheight: 14.9rem;
-    background-color: ${theme.colors.gray[0]};
-    border-radius: 1.6rem;
+	width: 90.3rem;
+	hheight: 14.9rem;
+	background-color: ${theme.colors.gray[0]};
+	border-radius: 1.6rem;
 `;
 
 const ModalHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const Header = styled.div`
-    font-size: ${theme.fontSize.xlarge};
-    font-weight: ${theme.font.medium.fontWeight};
-    color: ${theme.colors.gray[900]};
-    line-height: 133.333%;
-    letter-spacing: -0.0456rem;
-    margin-top: 2.1rem;
-    margin-left: 2.8rem;
-    margin-bottom: 2.9rem;
+	font-size: ${theme.fontSize.xlarge};
+	font-weight: ${theme.font.medium.fontWeight};
+	color: ${theme.colors.gray[900]};
+	line-height: 133.333%;
+	letter-spacing: -0.0456rem;
+	margin-top: 2.1rem;
+	margin-left: 2.8rem;
+	margin-bottom: 2.9rem;
 `;
 
 const StyledCloseIcon = styled(CloseIcon)`
-    margin-right: 1.8rem;
-    margin-bottom: 2rem;
-    width: 2.6rem;
-    height: 2.6rem;
-    cursor: pointer;
+	margin-right: 1.8rem;
+	margin-bottom: 2rem;
+	width: 2.6rem;
+	height: 2.6rem;
+	cursor: pointer;
 `;
 
 const Content = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const LinkText = styled.span`
-    font-size: ${theme.fontSize.large};
-    font-weight: ${theme.font.medium.fontWeight};
-    color: ${theme.colors.gray[900]};
-    line-height: 100%;
-    letter-spacing: -0.0608rem;
-    word-break: break-all;
-    margin-left: 7.4rem;
-    margin-bottom: 3.5rem;
+	font-size: ${theme.fontSize.large};
+	font-weight: ${theme.font.medium.fontWeight};
+	color: ${theme.colors.gray[900]};
+	line-height: 100%;
+	letter-spacing: -0.0608rem;
+	word-break: break-all;
+	margin-left: 7.4rem;
+	margin-bottom: 3.5rem;
+
+	@media (max-width: 480px) {
+		font-size: 1.9rem;
 `;
 
 const CopyButton = styled.button`
-    font-size: ${theme.fontSize.xlarge};
-    font-weight: ${theme.font.medium.fontWeight};
-    color: ${theme.colors.gray[900]};
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    margin-right: 4rem;
-    margin-bottom: 3.5rem;
+	font-size: ${theme.fontSize.xlarge};
+	font-weight: ${theme.font.medium.fontWeight};
+	color: ${theme.colors.gray[900]};
+	background-color: transparent;
+	border: none;
+	cursor: pointer;
+	margin-right: 4rem;
+	margin-bottom: 3.5rem;
+
+	@media (min-width: 768px) and (max-width: 1024px) {
+		font-size: ${theme.fontSize.large};
+	}
+	@media (min-width: 480px) and (max-width: 768px) {
+		font-size: ${theme.fontSize.large};
+	}
+	@media (max-width: 480px) {
+		font-size: ${theme.fontSize.medium};
+	}
 `;
