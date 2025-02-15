4e43a76839889b6ae3f7b1cba7de314fd34c9e51
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2025-02-15 23:55:56 +0900

    style : Webkit 이슈 수정

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -6,9 +6,8 @@ const GlobalStyle = createGlobalStyle`
     font-size: 62.5%;
     font-family: "Pretendard"
   }
-  button, input, select {
+  button {
     font-family: "Pretendard";
-    -webkit-appearance: none; /* 웹킷 브라우저에서 기본 스타일 제거 */
   }
 
   body {
